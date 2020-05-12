40d3f66ba8a2ee3e377971f194920949955c9c9b
Benjamin Köhler <benjaminkoehler89@gmail.com>
2020-05-12 18:27:34 +0200

    validate credit card numbers

---
+++ src/main/java/CreditCardValidator.java
@@ -0,0 +1,38 @@
+public class CreditCardValidator {
+
+    public boolean validate(String creditCardNumber) {
+        if(creditCardNumber.length() < 13 || creditCardNumber.length() > 16) {
+            return false;
+        }
+        if(!(creditCardNumber.startsWith("4") ||
+                creditCardNumber.startsWith("5") ||
+                creditCardNumber.startsWith("37") ||
+                creditCardNumber.startsWith("6"))) {
+            return false;
+        }
+
+        var reversed = new StringBuilder((creditCardNumber)).reverse().toString();
+        int sum = calculateSum(reversed);
+
+        return sum % 10 == 0;
+    }
+
+    private int calculateSum(String reversed) {
+        var sum = 0;
+        for(int i = 0; i < reversed.length(); i++){
+            var number = Character.getNumericValue(reversed.charAt(i));
+
+            if((i + 1) % 2 == 0) {
+                var doubled = number * 2;
+                if (doubled >= 10) {
+                    sum += (doubled % 10) + 1;
+                } else {
+                    sum += doubled;
+                }
+            } else {
+                sum += number;
+            }
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/CreditCardValidatorTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreditCardValidatorTest {
+    CreditCardValidator cut = new CreditCardValidator();
+
+    @Test
+    public void positiveTests() {
+        assertTrue(cut.validate("4242424242424242"));
+        assertTrue(cut.validate("4000056655665556"));
+        assertTrue(cut.validate("5555555555554444"));
+        assertTrue(cut.validate("5200828282828210"));
+        assertTrue(cut.validate("5105105105105100"));
+        assertTrue(cut.validate("378282246310005"));
+        assertTrue(cut.validate("371449635398431"));
+        assertTrue(cut.validate("6011111111111117"));
+        assertTrue(cut.validate("6011000990139424"));
+        assertTrue(cut.validate("6200000000000005"));
+    }
+
+    @Test
+    public void wrongLength() {
+        assertFalse(cut.validate("799273987132"));
+        assertFalse(cut.validate("79927398713646322"));
+    }
+
+    @Test
+    public void wrongPrefix() {
+        assertFalse(cut.validate("2223003122003222"));
+        assertFalse(cut.validate("3056930009020004"));
+        assertFalse(cut.validate("36227206271667"));
+        assertFalse(cut.validate("3566002020360505"));
+    }
+
+    @Test
+    public void failedLuhnTest() {
+        assertFalse(cut.validate("4242424242424252"));
+        assertFalse(cut.validate("4000056655665576"));
+        assertFalse(cut.validate("5555555555554424"));
+        assertFalse(cut.validate("5200828282833310"));
+        assertFalse(cut.validate("5105105105115100"));
+        assertFalse(cut.validate("378282246318005"));
+        assertFalse(cut.validate("371449635392431"));
+        assertFalse(cut.validate("6011111111191117"));
+        assertFalse(cut.validate("6011000990129424"));
+        assertFalse(cut.validate("6200000000010005"));
+    }
+}
